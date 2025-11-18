26cf0e6696fbda3fabd74e90f3f6fae5237fb7ce
wwwabijithu1234-lgtm <www.abijithu1234@gmail.com>
2025-11-18 14:53:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Movie:<db_password>@rocky.8rr5mys.mongodb.net/?appName=Rocky")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jithuabi619_db_user:<db_password>@cluster0.tkyuhfu.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collection name (default: dreamcinezone_files)
 
